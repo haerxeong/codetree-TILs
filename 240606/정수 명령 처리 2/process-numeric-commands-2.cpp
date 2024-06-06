8c535b7db1e10f75c7700d5253dc88092116c954
haerxeong <144093044+haerxeong@users.noreply.github.com>
2024-06-06 16:45:26 +0900

    Title: 정수 명령 처리 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240606/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Queue {
+private:
+    int q[5];
+    int front, rear;
+
+public:
+    Queue();
+    void push(int a);
+    int pop();
+    int size();
+    bool empty();
+    int _front();
+};
+
+Queue::Queue() : q{}, front(-1), rear(-1) {}
+
+void Queue::push(int a) {
+    if (empty()) {
+        front = 0;
+    }
+    q[++rear] = a;
+}
+
+int Queue::pop() {
+    int value = q[front];
+    if (front == rear) {
+        front = rear = -1; // Reset queue if it becomes empty
+    } else {
+        front++;
+    }
+    return value;
+}
+
+int Queue::size() {
+    if (empty()) return 0;
+    return rear - front + 1;
+}
+
+bool Queue::empty() {
+    return front == -1;
+}
+
+int Queue::_front() {
+    return q[front];
+}
+
+int main() {
+    Queue q;
+    int n, x;
+    string input;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        if (input == "push") {
+            cin >> x;
+            q.push(x);
+        } 
+        else if (input == "pop") {
+            cout << q.pop() << endl;
+        } 
+        else if (input == "size") {
+            cout << q.size() << endl;
+        } 
+        else if (input == "empty") {
+            cout << (int)q.empty() << endl;
+        } 
+        else if (input == "front") {
+            cout << q._front() << endl;
+        }
+    }
+    return 0;
+}
